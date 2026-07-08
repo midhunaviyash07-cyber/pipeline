f2cef0bc8d713d8cff0b7b0ad120566c18bee158
Midhuna S <midhunaviyash07@gmail.com>
2026-07-08 20:19:33 +0530

    Fix: Update divide error message to match test expectations

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => {
-  if (b === 0) throw new Error('Division by zero');
+  if (b === 0) throw new Error('Division by zero is not allowed');
   return a / b;
 };
 
